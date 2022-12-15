a3e11b69bd023aee869589ddb165ddb37ded2409
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-15 22:57:17 +0600

    two things updated
    - removed prototype of `display()`
    - create new method `find_prefix_sums()`

--- prefix_sum_and_different_arrays/theory.cpp
+++ prefix_sum_and_different_arrays/theory.cpp
@@ -3,8 +3,6 @@
 
 using namespace std;
 
-void display(vector<int> b);
-
 vector<int> findPrefixSums(vector<int> a)
 {
     int n = a.size();
@@ -19,16 +17,29 @@ vector<int> findPrefixSums(vector<int> a)
 
 }
 
-void display(vector<int> b)
+vector<int> find_prefix_sums(vector<int>& array)
+{
+    int size = array.size();
+    vector<int> prefixSums(size + 1, 0);
+    for(int i=0; i<size; i++)
+    {
+        prefixSums[i+1] = prefixSums[i] + array[i];
+    }
+
+    return prefixSums;
+}
+
+void display(string str, vector<int> b)
 {
 
     int n = b.size();
+    cout << str << "=[ ";
     for(int i=0; i<n; i++)
     {
         cout << b[i] << " ";
     }
 
-    cout << endl;
+    cout << "]" << endl;
 }
 
 int main()
@@ -37,7 +48,10 @@ int main()
     
     vector<int> b = findPrefixSums(arr);
 
-    display(b);
+    display("b", b);
+
+    vector<int> pb = find_prefix_sums(arr);
+    display("pb", pb);
     
     return 0;
 }
