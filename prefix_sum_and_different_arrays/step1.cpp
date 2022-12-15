e5233ef3ccb185f7c4de40b3a670b6a5bad96eec
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-16 00:10:55 +0600

    Update step1.cpp

--- prefix_sum_and_different_arrays/step1.cpp
+++ prefix_sum_and_different_arrays/step1.cpp
@@ -20,17 +20,18 @@ vector<int> prefixSum_TO_originalArray(vector<int> arr)
 void display(vector<int> orr)
 {
     int n = orr.size();
-
+    cout << "[ ";
     for(int i=0; i<n; i++)
     {
         cout << orr[i] << " ";
     }
-    cout << endl;
+    cout << "]"<< endl;
 }
 
 int main()
 {
-    vector<int> arr = {0, 1, 5, 3, 7, 7, -2, 1};
+    vector<int> arr = {0, 1, 5, 3, 7, 7, -2, 1}; // output:: [ 1 4 -2 4 0 -9 3 ]
+    //vector<int> brr = { 0, 5, 9, 16, 18, 20, 19, 27 }; // output :: [ 5 4 7 2 2 -1 8 ]
 
     vector<int> oarr = prefixSum_TO_originalArray(arr);
     display(oarr);
