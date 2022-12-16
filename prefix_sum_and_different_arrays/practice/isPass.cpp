be3e8f270e64859b39c7452498791370795d8750
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-16 23:21:01 +0600

    Create isPass.cpp

---
+++ prefix_sum_and_different_arrays/practice/isPass.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> findPrefixSums(vector<int> a)
+{
+    int n = a.size();
+    vector<int> b(n+1, 0);
+    for(int i=0; i<n; i++)
+    {
+        b[i+1] = b[i] + a[i];
+    }
+
+    return b;
+}
+
+void display(vector<int> b)
+{
+
+    int n = b.size();
+    for(int i=0; i<n; i++)
+    {
+        cout << b[i] << " ";
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> a(n, 0);
+
+    for(int i=0; i<n; i++)
+    {
+        cin >> a[i];
+    }
+
+    vector<int> b = findPrefixSums(a);
+    display(b);
+    
+    return 0;
+}
