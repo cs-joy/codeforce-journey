2b12d60e9408e8b049d718d659815bbb874dd5b5
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-17 15:16:41 +0600

    Update theory.cpp

--- prefix_sum_and_different_arrays/step2/theory.cpp
+++ prefix_sum_and_different_arrays/step2/theory.cpp
@@ -3,17 +3,45 @@
 
 using namespace std;
 
-int getSum(int b[], int l, int r)
+int getSum(vector<int> b, int l, int r)
 {
     return b[r] - b[l];
 }
 
+vector<int> pRs(vector<int> b)
+{
+    int n = b.size();
+    vector<int> a(n+1, 0);
+    for(int i=0; i<n; i++)
+    {
+        a[i+1] = a[i] + b[i];
+    }
+
+    return a;
+}
+
+void dis(vector<int> b)
+{
+    int n = b.size();
+    for(int i=0; i<n; i++)
+    {
+        cout << b[i] << " ";
+    }
+    cout << endl;
+}
+
 int main()
 {
-    int a[] = {2, 4, 6, 7, 8, 9, 1};
+    vector<int> a = {2, 4, 6, 7, 8, 9, 1};
 
-   
-    cout << getSum(a, 2, 3);
+
+    dis(pRs(a));
+     
+    int left = 2;
+    int right = 6;
+    
+
+    cout << getSum(pRs(a), left, right);
     
     return 0;
 }
