714b03c40da48cdbe24464ce97b87a0aa6f77b5c
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-02-19 22:33:37 +0600

    Create 1795A.cpp

---
+++ problems/1795A.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t>0)
+    {
+        t--;
+
+        int n, m;
+        cin >> n >> m;
+
+        char s[n];
+
+        for (int i=0; i<n; i++)
+        {
+            cin >> s[i];
+        }
+
+        char t[m];
+        for (int j=0; j<m; j++)
+        {
+            cin >> t[j];
+        }
+
+    }
+
+    return 0;
+}
