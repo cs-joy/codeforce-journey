a252494e9b5819cf4e7ae2b9530d3ab62bf490d0
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-02-20 16:24:57 +0600

    almost
    95A ,, wrong with `1 2 R BB`

--- problems/1795A.cpp
+++ problems/1795A.cpp
@@ -28,6 +28,8 @@ int main()
         }
 
         bool isBeautifull;
+        char chr;
+        int ct=0;
 
         for (int i=0; i<n; i++)
         {
@@ -35,6 +37,14 @@ int main()
             {
                 if (s[i] == s[j])
                 {
+                    ct++;
+                    if (ct == 1)
+                    {
+                        t.push_back(s[i]);
+                        break;
+                    }
+                    //chr = s[i];
+                    /*
                     if (s[i] != t[m-1])
                     {
                         isBeautifull= true;
@@ -42,10 +52,15 @@ int main()
                     else
                     {
                         isBeautifull = false;
-                    }
+                    }*/
                 }
             }
         }
+
+        t.push_back(chr);
+
+        char ch;
+        int cnt=0;
 
         for (int i=0; i<m; i++)
         {
@@ -53,6 +68,15 @@ int main()
             {
                 if (t[i] == t[j])
                 {
+                    cnt++;
+                    if (cnt == 1)
+                    {
+                        s.push_back(t[i]);
+                        break;
+                    }
+
+                    //ch = t[i];
+                    /*
                     if (t[i] != s[n-1])
                     {
                         isBeautifull= true;
@@ -60,12 +84,25 @@ int main()
                     else
                     {
                         isBeautifull = false;
-                    }
+                    }*/
                 }
             }
         }
 
-        (isBeautifull) ? cout << "YES\n" : cout << "NO\n";
+        //s.push_back(ch);
+
+        //(isBeautifull) ? cout << "YES\n" : cout << "NO\n";
+        cout << "t= ";
+        for (int i=0; i<t.size(); i++)
+        {
+            cout << t[i] << " ";
+        }
+
+        cout << "s= ";
+        for (int i=0; i<s.size(); i++)
+        {
+            cout << s[i] << " ";
+        }
 
     }
 
---
+++ problems/95A.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t>0)
+    {
+        t--;
+
+        int n, m;
+        cin >> n >> m;
+
+        vector<char> s(n);
+
+        for (int i=0; i<n; i++)
+        {
+            cin >> s[i];
+        }
+
+        vector<char> t(m);
+        for (int j=0; j<m; j++)
+        {
+            cin >> t[j];
+        }
+
+        int c;
+        char ch, pos;
+
+        for (int i=0; i<n; i++)
+        {
+            for (int j=i+1; j<n; j++)
+            {
+                if (s[i] == s[j])
+                {
+                    c=1;
+                    ch = s[i];
+                    pos = i;
+                }
+                else
+                {
+                    c=0;
+                }
+            }
+        }
+
+        if (c == 1)
+        {
+            t.push_back(ch);
+            s.pop_back();
+        }
+
+        bool f_beautifull = false;
+        if (s[s.size() - 1] != s[s.size() - 2])
+        {
+            f_beautifull = true;
+        }
+
+
+        bool t_beautifull = false;
+        if (t[t.size() - 1] != t[t.size() - 2])
+        {
+            t_beautifull = true;
+        }
+
+        if (f_beautifull)
+        {
+            if (t_beautifull)
+            {
+                cout << "YES" << endl;
+            }
+            else
+            {
+                cout << "NO" << endl;
+            }
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+
+    }
+    return 0;
+}
+
